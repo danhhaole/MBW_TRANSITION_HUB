0dedaec525fbe25b332bb1957ee343681990c71a
danhhaole <danhhao.le@gmail.com>
2025-10-27 15:14:53 +0700

    haold update

--- mbw_mira/hooks.py
+++ mbw_mira/hooks.py
@@ -86,8 +86,8 @@ add_to_apps_screen = [
 # ------------
 
 # before_install = "mbw_mira.install.before_install"
-after_install = "mbw_mira.install.after_install"
-after_migrate = "mbw_mira.install.install_test"
+# after_install = "mbw_mira.install.after_install"
+# after_migrate = "mbw_mira.install.install_test"
 
 # Uninstallation
 # ------------
