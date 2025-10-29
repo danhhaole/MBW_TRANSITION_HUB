bef20cfd16269ecd5297fe98569f4648edaa140d
danhhaole <danhhao.le@gmail.com>
2025-10-29 08:33:26 +0700

    haold update mira access token

---
+++ mbw_mira/mbw_mira/doctype/mira_access_token/test_mira_access_token.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, mbwcloud.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMiraAccessToken(FrappeTestCase):
+	pass
